6d96d4e09831f5af6bad79b234549513d9718c1c
tiiedye <tiiedye@gmail.com>
2022-01-21 09:39:41 -0800

    setting up sheety environment and post, data now shows on google sheet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
+from datetime import datetime
 
 load_dotenv()
 
@@ -8,7 +9,7 @@ nutritionix_ID = os.getenv('NUTRITIONIX_ID')
 nutritionix_key = os.getenv('NUTRITIONIX_KEY')
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-headers = {
+nutritionix_headers = {
     "x-app-id": nutritionix_ID,
     "x-app-key": nutritionix_key,
     "Content-Type": "application/json"
@@ -30,7 +31,33 @@ exercise_params = {
     "age": age
 }
 
-exercise_response = requests.post(url=exercise_endpoint, json=exercise_params, headers=headers)
+exercise_response = requests.post(url=exercise_endpoint, json=exercise_params, headers=nutritionix_headers)
 exercise_response.raise_for_status()
 result = exercise_response.json()
-print(result)
+
+# NOTE: User's will need to set up their own sheety account and workout spreadsheet
+sheety_post_url = os.getenv('SHEETY_POST_URL')
+sheety_token = os.getenv('SHEETY_TOKEN')
+
+now = datetime.now()
+date = now.strftime("%m/%d/%Y")
+time = now.strftime("%X")
+
+sheety_headers = {
+    "Authorization": f"Bearer {sheety_token}"
+}
+
+for exercise in result['exercises']:
+    sheety_params = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": exercise['name'].title(),
+            "duration": exercise['duration_min'],
+            "calories": exercise['nf_calories']
+        }
+    }
+
+    sheety_post = requests.post(url=sheety_post_url, json=sheety_params, headers=sheety_headers)
+    sheety_post.raise_for_status()
+    print(sheety_post.text)
